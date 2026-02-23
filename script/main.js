bc68a6ad59d75617f61ca88b72baaa51d3ae71da
stefanosorrenti <stefanosorrenti01@gmail.com>
2026-02-23 15:48:04 +0100

    add humna code

---
+++ script/main.js
@@ -0,0 +1,32 @@
+//TRACCIA
+
+/*
+Descrizione
+
+Attraverso l'apposita API di Boolean
+https://flynn.boolean.careers/exercises/api/random/mail
+generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+
+Bonus
+
+Abbellire con CSS o Bootstrap
+Inserire un bottone che al click faccia il fetch altre 10 mail (sostituendo le altre)
+
+Buon divertimento e confermate lettura come al solito :babyyoda:
+
+*/
+
+//HUMAN CODE
+
+/*
+Cosa devo fare?
+-Salvare il endpoint
+-Selezionarmi la chiave email
+-Creare una lista HTML
+-Loopare per 10 volte la chiave
+-Inserirla nella  lista
+
+
+BONUS:
+
+*/
